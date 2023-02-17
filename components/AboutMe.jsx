9a94f13f4c81f6c41e62ac4eae3f1d4d79c40abf
vukovicdusan <dusan.v.vukovic@gmail.com>
2023-02-17 12:15:53 +0100

    shapedivider added on hero section top

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -11,7 +11,7 @@ import Title from "./Title"
 const AboutMe = () => {
 	return (
 		<div className={styles.background}>
-			<div className={styles.shapeDivider}>
+			<div className={styles.shapedivider}>
 				<svg
 					data-name="Layer 1"
 					xmlns="http://www.w3.org/2000/svg"
