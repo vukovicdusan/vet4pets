68e0feac50a7a5fcde92487c690770bc75550bf4
Picvajzmen <picvajzmen@gmail.com>
2023-09-14 11:46:24 +0200

    Added testimonials from google reviews

--- components/testimonials/testimonials-content.js
+++ components/testimonials/testimonials-content.js
@@ -1,20 +1,22 @@
 export const testimonials = [
   {
-    name: "Milan Đorđević",
-    title: "Lorem ipsum dolor sit amet",
+    name: "Marija Denić",
     content:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa corporis est alias perferendis et dicta nihil sapiente fuga aut neque!",
+      "Izutetno profesionalni, visoko strucni, veoma posveceni svom poslu, komunikativni i pozrtvovani. Ambulanta opremljena sa svim potrebnim stvarima. Savrseno mesto za vase ljubimce sve preporuke. Moj pas ih obozava, nema strah kao sto je imao pre kad smo vodili kod drugih veterinara i dobija posle svakog pregleda nagradu tj. slatkis.",
   },
   {
-    name: "Jane Maričić",
-    title: "Lorem ipsum dolor sit amet",
+    name: "Una Vidaković",
     content:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa corporis est alias perferendis et dicta nihil sapiente fuga aut neque!",
+      "Ovo su ljudi koji istinski vole ono sto rade, imaju puno iskustva i stvarno su sjajni u svom poslu. Oni su nam bukvalno za sat vremena mnogo olaksali zivot i ustedeli poprilicno para. Mi smo ih pozvali da se samo na brzinu raspitamo rade li mozda neke komplikovanije analize (pritom je bila subota, pola 5 popodne), a dobili smo puno iskusnih i dobrih saveta. Uradili su sve analize koje su nam trebale, sve su uradili krajnje profesionalno, a onda su nam jos bar 15-20 min (i to nakon sto smo platili) pricali sta su radili drugi ljudi u nasoj situaciji i savetovali nas kako da najlakse zavrsimo stvari dalje. Sve preporuke!!!",
   },
   {
-    name: "Branislav Trifunović",
-    title: "Lorem ipsum dolor sit amet",
+    name: "Dušan Vuković",
     content:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa corporis est alias perferendis et dicta nihil sapiente fuga aut neque!",
+      "Može se ipak još uvek naći neko ko iskreno, ljudski i kvalitetno radi svoj posao. Moja preporuka.",
+  },
+  {
+    name: "Đorđe Radić",
+    content:
+      "Čovek je veoma posvećen svom poslu i zaista ga radi da bi pomogao životinjama. Nikad nije pokušao nešto da uvali i proda tek tako, pun dobrih i održivih saveta oko lekova i potrepština za životinje. Topla preporuka.",
   },
 ];
