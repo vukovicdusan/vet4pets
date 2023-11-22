5a7b253a1733903bd114e8a9b582002853b63986
vukovicdusan <dusan.v.vukovic@gmail.com>
2023-11-22 22:13:56 +0100

    Added scroll when clicking on a link from a different page

--- components/layout/header/Header.jsx
+++ components/layout/header/Header.jsx
@@ -4,91 +4,115 @@ import * as styles from "./Header.module.css";
 import Link from "next/link";
 import Image from "next/image";
 import logo from "@/public/img/logo-220.png";
+import { useRouter } from "next/router";
 
 const Header = () => {
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-  const [scrollHeight, setScrollHeight] = useState(0);
+	const [dropdownOpen, setDropdownOpen] = useState(false);
+	const [scrollHeight, setScrollHeight] = useState(0);
+	const [mounted, setMounted] = useState(false);
 
-  useEffect(() => {
-    window.addEventListener("scroll", scrollHandler);
-    return () => {
-      window.removeEventListener("scroll", scrollHandler);
-    };
-  }, []);
+	let router = useRouter();
 
-  const dropdownOpenHandler = () => {
-    dropdownOpen === false ? setDropdownOpen(true) : setDropdownOpen(false);
-  };
+	useEffect(() => {
+		let targetId = router.asPath.replace(/.*\#/, "");
+		const elem = document.getElementById(targetId);
+		setMounted(true);
+		window.scrollTo({
+			top: elem?.getBoundingClientRect().top,
+			behavior: "smooth",
+		});
+	}, [mounted]);
 
-  const scrollHandler = () => {
-    const currentScrollHeight = window.scrollY;
-    setScrollHeight(currentScrollHeight);
-  };
+	useEffect(() => {
+		window.addEventListener("scroll", scrollHandler);
+		return () => {
+			window.removeEventListener("scroll", scrollHandler);
+		};
+	}, []);
 
-  return (
-    <div>
-      <header
-        className={`${styles.header} ${
-          scrollHeight > 0 ? styles.boxShadow : ""
-        }`}
-      >
-        <Wrapper>
-          <div className={styles.headerWrap}>
-            <Link className={styles.headerLogo} href={"/"}>
-              <Image
-                src={logo}
-                width={50}
-                height={50}
-                className={logo}
-                alt={logo}
-                priority
-              ></Image>
-            </Link>
-            <nav className={styles.desktopNav}>
-              <ul className={styles.headerWrap}>
-                <li>
-                  <Link href={"/"}>Početna</Link>
-                </li>
-                <li
-                  onClick={dropdownOpenHandler}
-                  className={`${styles.dropdown} ${
-                    dropdownOpen ? `${styles.dropdownOpen}` : ""
-                  }`}
-                >
-                  Kalkulatori
-                  <span>
-                    <svg className={styles.chevron}>
-                      <use xlinkHref={"./img/sprite.svg#triangle"}></use>
-                    </svg>
-                  </span>
-                  <ul className={styles.dropdownContent}>
-                    <li>
-                      <Link href={"./kalkulator-kalorija"}>
-                        Kalkulator kalorija
-                      </Link>
-                    </li>
-                    <li>
-                      <Link href={"./kalkulator-cokolada"}>
-                        Kalkulator za trovanje čokoladom
-                      </Link>
-                    </li>
-                    <li>
-                      <Link href={"./kalkulator-rast"}>
-                        Kalkulator veličine psa
-                      </Link>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <a href="./#contact">Kontakt</a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </Wrapper>
-      </header>
-    </div>
-  );
+	const dropdownOpenHandler = () => {
+		dropdownOpen === false ? setDropdownOpen(true) : setDropdownOpen(false);
+	};
+
+	const scrollHandler = () => {
+		const currentScrollHeight = window.scrollY;
+		setScrollHeight(currentScrollHeight);
+	};
+
+	return (
+		<div>
+			<header
+				className={`${styles.header} ${
+					scrollHeight > 0 ? styles.boxShadow : ""
+				}`}
+			>
+				<Wrapper>
+					<div className={styles.headerWrap}>
+						<Link className={styles.headerLogo} href={"/"}>
+							<Image
+								src={logo}
+								width={50}
+								height={50}
+								className={logo}
+								alt={logo}
+								priority
+							></Image>
+						</Link>
+						<nav className={styles.desktopNav}>
+							<ul className={styles.headerWrap}>
+								<li>
+									<Link href={"/"}>Početna</Link>
+								</li>
+								<li
+									onClick={dropdownOpenHandler}
+									className={`${styles.dropdown} ${
+										dropdownOpen
+											? `${styles.dropdownOpen}`
+											: ""
+									}`}
+								>
+									Kalkulatori
+									<span>
+										<svg className={styles.chevron}>
+											<use
+												xlinkHref={
+													"./img/sprite.svg#triangle"
+												}
+											></use>
+										</svg>
+									</span>
+									<ul className={styles.dropdownContent}>
+										<li>
+											<Link
+												href={"./kalkulator-kalorija"}
+											>
+												Kalkulator kalorija
+											</Link>
+										</li>
+										<li>
+											<Link
+												href={"./kalkulator-cokolada"}
+											>
+												Kalkulator za trovanje čokoladom
+											</Link>
+										</li>
+										<li>
+											<Link href={"./kalkulator-rast"}>
+												Kalkulator veličine psa
+											</Link>
+										</li>
+									</ul>
+								</li>
+								<li>
+									<a href="./#contact">Kontakt</a>
+								</li>
+							</ul>
+						</nav>
+					</div>
+				</Wrapper>
+			</header>
+		</div>
+	);
 };
 
 export default Header;
