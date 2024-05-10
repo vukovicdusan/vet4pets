0587cfdfdae3b0ea6e77cf455cc2b01b15ca6332
Picvajzmen <picvajzmen@gmail.com>
2024-05-10 11:13:29 +0200

    Added footer credits link

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -1,19 +1,26 @@
-import React from "react"
-import Wrapper from "./Wrapper"
-import * as styles from "../../styles/Footer.module.css"
+import React from "react";
+import Wrapper from "./Wrapper";
+import * as styles from "../../styles/Footer.module.css";
 
 const Footer = () => {
-	let year = new Date().getFullYear()
-	return (
-		<footer className={styles.footer}>
-			<Wrapper>
-				<p className={styles.footerCenter}>
-					{" "}
-					Copyright © {year} vet4Pets | Website by Dušan Vuković{" "}
-				</p>
-			</Wrapper>
-		</footer>
-	)
-}
+  let year = new Date().getFullYear();
+  return (
+    <footer className={styles.footer}>
+      <Wrapper>
+        <p className={styles.footerCenter}>
+          {" "}
+          Copyright © {year} vet4Pets | Website by{" "}
+          <a
+            href="https://www.dusanvukovic.com/"
+            target="__blank"
+            rel="nooopener"
+          >
+            Dušan Vuković{" "}
+          </a>
+        </p>
+      </Wrapper>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
